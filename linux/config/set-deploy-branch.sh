90697aa0c09997905ecf52eb67cb557e1752d546
50U10FCA7 <31173478+50U10FCA7@users.noreply.github.com>
2018-09-27 01:06:34 +0300

    fix '' ""

--- linux/config/set-deploy-branch.sh
+++ linux/config/set-deploy-branch.sh
@@ -8,13 +8,13 @@ fi
 
 gman="./../gman.sh"
 writeline="./../utils/wline.sh"
-bash $writeline $gman 22 "config_branch='$branch'"
+bash $writeline $gman 22 "config_branch=\"$branch\""
 
 cd $config_repositories
 for repository in */; do
     cd $repository
     #addon=$config_gmod"/garrysmod/addons/"$(cat addon)
-    bash $writeline ./hooks/post-receive 5 "branch='$branch'"
+    bash $writeline ./hooks/post-receive 5 "branch=\"$branch\""
     #git worktree add $addon $branch
     cd $config_repositories
 done
