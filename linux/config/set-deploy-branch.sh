1661af8e800cbea9409635fceb56397f2c26304a
50U10FCA7 <31173478+50U10FCA7@users.noreply.github.com>
2018-09-27 01:25:19 +0300

    clean

--- linux/config/set-deploy-branch.sh
+++ linux/config/set-deploy-branch.sh
@@ -13,8 +13,6 @@ bash $writeline $gman 22 "config_branch=\"$branch\""
 cd $config_repositories
 for repository in */; do
     cd $repository
-    #addon=$config_gmod"/garrysmod/addons/"$(cat addon)
     bash $writeline ./hooks/post-receive 6 "branch=\"$branch\""
-    #git worktree add $addon $branch
     cd $config_repositories
 done
